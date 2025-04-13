e2811962bbc05ff28f925a14b1e11d4398f95b67
echoview2 <nacsxelf071s@gmail.com>
2025-04-13 08:41:09 +0000

    Fix PATH export in setup.sh to append to .bashrc

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ sudo apt-get install -y sl
 
 # Add /usr/games to PATH if not already present
 if [[ ":$PATH:" != *":/usr/games:"* ]]; then
-    export PATH="$PATH:/usr/games"
+    export PATH="$PATH:/usr/games" >> ~/.
 fi
